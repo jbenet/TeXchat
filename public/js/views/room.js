0d47aee4b4c911b373e541b5bfd063ddab1ddb5c
jbenet <jbenet@cs.stanford.edu>
2012-01-17 14:54:22 -0800

    added test messages

--- public/js/views/room.js
+++ public/js/views/room.js
@@ -33,6 +33,11 @@ var RoomView = Backbone.View.extend({
     this.chatView.render();
     this.previewView.render();
     // this.userlistView.render();
+
+    for (var i = 0; i < 100; i++) {
+      this.elSendBox().val('herp ' + i + ' + ' + i + ' = ' + (i+i));
+      this.sendMessage();
+    }
   },
 
   sendMessage: function() {
